0f07944b09c72616a0a572ed5290f7fad9d29122
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-07 13:39:34 +0300

    include NotesGrid and NotesEditor to App

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NotesEditor from './../NotesEditor/NotesEditor';
+import NotesGrid from './../NotesGrid/NotesGrid';
 import 'normalize.css';
 import './style.css';
 
@@ -7,8 +8,10 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="app">
+                <h2 className="app__header">Notes App</h2>
                 <NotesEditor />
+                <NotesGrid />
             </div>
         );
     }
