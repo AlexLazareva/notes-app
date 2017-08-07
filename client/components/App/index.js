4d272e0dc026e5207aff0617845839dc65cb11c0
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-07 06:28:15 +0300

    create App component and include styles

---
+++ client/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'normalize.css';
+import './style.css';
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <h1>Notes</h1>
+
+        );
+    }
+}
