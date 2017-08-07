0c6d42d9f8468ccf2a7d0641f6db7e788511c934
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-07 19:18:50 +0300

    create input and textarea methods

--- client/components/NotesEditor/NotesEditor.js
+++ client/components/NotesEditor/NotesEditor.js
@@ -13,6 +13,14 @@ const NotesEditor = React.createClass(
             }
         },
 
+        handleTitleChange(event) {
+            this.setState({ title: event.target.value });
+        },
+
+        handleTextChange(event) {
+            this.setState({ text: event.target.value });
+        },
+
         render() {
             return (
                 <div className="notesEditor">
@@ -21,16 +29,21 @@ const NotesEditor = React.createClass(
                         className="notesEditor__title"
                         placeholder="Enter note title"
                         value={ this.state.title }
+                        onChange={ this.handleTitleChange }
                     />
                     <textarea
                         placeholder="Enter note text"
                         rows={5}
                         className="notesEditor__text"
                         value={ this.state.text }
+                        onChange={ this.handleTextChange }
                     />
                     <div className="notesEditor__footer">
                         <ColorPicker/>
-                        <button className="notesEditor__button">Add</button>
+                        <button
+                            className="notesEditor__button"
+                            disabled={ !this.state.text }
+                        >Add</button>
                     </div>
                 </div>
             );
