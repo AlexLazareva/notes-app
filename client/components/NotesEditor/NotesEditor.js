c2de178d46970aba28a20a1decb8671a5ceda07d
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-08 07:32:48 +0300

    fix colorpicker

--- client/components/NotesEditor/NotesEditor.js
+++ client/components/NotesEditor/NotesEditor.js
@@ -2,71 +2,71 @@ import React from 'react';
 import ColorPicker from './../ColorPicker/ColorPicker';
 import './style.scss';
 
-const NotesEditor = React.createClass(
-    {
-        getInitialState() {
-            return {
-                title: '',
-                text: '',
-                color: '#fff'
-            }
-        },
+const NoteEditor = React.createClass({
+    getInitialState() {
+        return {
+            title: '',
+            text: '',
+            color: '#FFFFFF'
+        };
+    },
 
-        handleTitleChange(event) {
-            this.setState({ title: event.target.value });
-        },
+    handleTextChange(event) {
+        this.setState({ text: event.target.value });
+    },
 
-        handleTextChange(event) {
-            this.setState({ text: event.target.value });
-        },
+    handleTitleChange(event) {
+        this.setState({ title: event.target.value });
+    },
 
-        handleNoteAdd() {
-            const newNote = {
-                title: this.state.title,
-                text: this.state.text,
-                color: this.state.color
-            };
+    handleColorChange(color) {
+        this.setState({ color });
+    },
 
-            this.props.onNoteAdd(newNote);
-            this.setState({ text: '', title: '', color: '#FFFFFF' });
-        },
+    handleNoteAdd() {
+        const newNote = {
+            title: this.state.title,
+            text: this.state.text,
+            color: this.state.color
+        };
 
-        handleColorChange() {
-            this.setState({ color });
-        },
+        this.props.onNoteAdd(newNote);
+        this.setState({ text: '', title: '', color: '#FFFFFF' });
+    },
 
-        render() {
-            return (
-                <div className="notesEditor">
-                    <input
-                        type="text"
-                        className="notesEditor__title"
-                        placeholder="Enter note title"
-                        value={ this.state.title }
-                        onChange={ this.handleTitleChange }
+    render() {
+        return (
+            <div className='notesEditor'>
+                <input
+                    type='text'
+                    className='notesEditor__title'
+                    placeholder='Enter title'
+                    value={this.state.title}
+                    onChange={this.handleTitleChange}
+                />
+                <textarea
+                    placeholder='Enter note text'
+                    rows={5}
+                    className='notesEditor__text'
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <div className='notesEditor__footer'>
+                    <ColorPicker
+                        value={this.state.color}
+                        onChange={this.handleColorChange}
                     />
-                    <textarea
-                        placeholder="Enter note text"
-                        rows={5}
-                        className="notesEditor__text"
-                        value={ this.state.text }
-                        onChange={ this.handleTextChange }
-                    />
-                    <div className="notesEditor__footer">
-                        <ColorPicker
-                            value={this.state.color}
-                            onChange={this.handleColorChange}
-                        />
-                        <button
-                            className="notesEditor__button"
-                            disabled={ !this.state.text }
-                            onClick={ this.handleNoteAdd }
-                        >Add</button>
-                    </div>
+                    <button
+                        className='notesEditor__button'
+                        disabled={!this.state.text}
+                        onClick={this.handleNoteAdd}
+                    >
+                        Add
+                    </button>
                 </div>
-            );
-        }
+            </div>
+        );
     }
-);
+});
 
-export default NotesEditor;
+export default NoteEditor;
