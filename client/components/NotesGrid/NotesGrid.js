a126f60624d7be0387f4eab83e5dafcb6a49b5e3
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-09 20:26:03 +0300

    include Note component

--- client/components/NotesGrid/NotesGrid.js
+++ client/components/NotesGrid/NotesGrid.js
@@ -16,7 +16,18 @@ const NotesGrid = React.createClass({
                 className='notesGrid'
                 options={masonryOptions}
             >
-
+                {
+                    this.props.notes.map(note =>
+                        <Note
+                            key={note.id}
+                            title={note.title}
+                            onDelete={this.props.onNoteDelete.bind(null, note)}
+                            color={note.color}
+                        >
+                            {note.text}
+                        </Note>
+                    )
+                }
             </Masonry>
         );
     }
