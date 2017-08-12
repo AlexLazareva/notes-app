77d4c9328ebcdaa8c067ac7c85c9f15b8877f898
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-12 08:38:20 +0300

    remove createClasss in the Note component

--- client/components/Note/Note.js
+++ client/components/Note/Note.js
@@ -1,16 +1,17 @@
 import React from 'react';
-
 import './styles.scss';
 
-const Note = React.createClass({
+export default class Note extends React.Component {
+
     render() {
         const style = {backgroundColor: this.props.color};
+
         return (
             <div className="note" style={style}>
                 <span className="note__del-icon" onClick={this.props.onDelete}> × </span>
                 {
                     this.props.title
-                    ?
+                        ?
                         <h4 className="note__title">{this.props.title}</h4>
                         :
                         null
@@ -19,6 +20,4 @@ const Note = React.createClass({
             </div>
         );
     }
-});
-
-export default Note;
+}
