d6b30794f62b941a2737470e3943b972e26a4b49
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-12 08:27:57 +0300

    remove createClasss Colorpicker

--- client/components/ColorPicker/ColorPicker.js
+++ client/components/ColorPicker/ColorPicker.js
@@ -2,28 +2,23 @@ import React from 'react';
 import cx from 'classnames';
 import './style.scss';
 
-const COLORS = ['#FFFFFF', '#80D8FF', '#FFFF8D', '#FF8A80', '#CCFF90', '#CFD8DC', '#FFD180'];
+export default class ColorPicker extends React.Component {
 
-const ColorPicker = React.createClass(
-    {
-
-        render() {
-            return (
-                <div className="colorPicker">
-                    {
-                        COLORS.map(color =>
-                            <div
-                                key={color}
-                                className={cx('colorPicker__swatch', { selected: this.props.value === color })}
-                                style={{ backgroundColor: color }}
-                                onClick={this.props.onChange.bind(null, color)}
-                            />
-                        )
-                    }
-                </div>
-            );
-        }
+    render() {
+        const COLORS = ['#FFFFFF', '#80D8FF', '#FFFF8D', '#FF8A80', '#CCFF90', '#CFD8DC', '#FFD180'];
+        return (
+            <div className="colorPicker">
+                {
+                    COLORS.map(color =>
+                        <div
+                            key={color}
+                            className={cx('colorPicker__swatch', { selected: this.props.value === color })}
+                            style={{ backgroundColor: color }}
+                            onClick={this.props.onChange.bind(null, color)}
+                        />
+                    )
+                }
+            </div>
+        );
     }
-);
-
-export default ColorPicker;
+}
